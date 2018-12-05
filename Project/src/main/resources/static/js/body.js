9add6ea92dd067f26d8d99e61a35333372589149
forget1228 <196800680@qq.com>
2018-12-05 09:59:48 +0800

    abk 2018-12-04 前台界面修改

--- Project/src/main/resources/static/js/body.js
+++ Project/src/main/resources/static/js/body.js
@@ -13,33 +13,66 @@ function findDate(){
     $.post(url.findDate,null,function (result) {
         if(result.code==0){
             console.info(result.data)
-            var html = "";
-            for(var i=0;i< result.data.length;i++) {
-                var ls = result.data[i];
-                if(ls.group_name == "www") {
-                    $("#template").append("<div class=\"col-md-4\">" +
-                        "   <div class=\"row\">" +
-                        "       <div class=\"thumbnail\">" +
-                        "           <img alt=" + ls.document_type + " src=\"static/image/" + ls.document_type + ".png\" height=\"60\" width=\"60\"/>" +
-                        "           <p>" + ls.template_name + "</p>" +
-                        "           <div class=\"caption\" style=\"display: none;\">" +
-                        "               <p></p>" +
-                        "               <p>" +
-                        "                   <a class=\"btn btn-primary\" href=\"javascript:void(0);\" onclick=\"template("+ls.abk_id+");\">下载</a>" +
-                        "                   <a data-toggle=\"modal\" href=\"#uploadModal\" class=\" btn btn-primary\"  >更新</a>" +
-                        "               </p>" +
-                        "           </div>" +
-                        "       </div>" +
-                        "   </div>" +
-                        "</div>");
-                    $(".thumbnail").hover(function () {
-                        $(this).children("div").css("display","block");
-                    }, function () {
-                        $(this).children("div").css("display","none");
-                    });
+            if(result.data.length > 0) {
+                var divStart = "<div class=\"row\">";
+                var divEnd = "</div>";
+                var div = "";
+                var group = "";
+                for (var i = 0; i < result.data.length; i++) {
+                    var ls = result.data[i];
+                    if(ls.group_name == "www"){
+                        div +=
+                            "   <div class=\"col-md-4\">" +
+                            "       <div class=\"thumbnail\">" +
+                            "           <img alt=" + ls.document_type + " src=\"static/image/" + ls.document_type + ".png\" height=\"60\" width=\"60\"/>" +
+                            "           <p>" + ls.template_name + "</p>" +
+                            "           <div class=\"caption\" style=\"display: none;\">" +
+                            "               <p></p>" +
+                            "               <p>" +
+                            "                   <a class=\"btn btn-primary\" href=\"javascript:void(0);\" onclick=\"template(" + ls.abk_id + ");\">下载</a>" +
+                            "                   <a data-toggle=\"modal\" href=\"#uploadModal\" class=\" btn btn-primary\"  >更新</a>" +
+                            "               </p>" +
+                            "           </div>" +
+                            "       </div>" +
+                            "   </div>";
+                        if ((i+1)%3==0&&(i<(result.data.length-1))) {
+                            group +=divStart+div+divEnd;
+                            div = "";
+                        }
+                        if(i == result.data.length-1){
+                            group = "<p><em>公共模板</em></em></p>" + group+divStart+div+divEnd;
+                        }
+                    }else {
+                        div +=
+                            "   <div class=\"col-md-4\">" +
+                            "       <div class=\"thumbnail\">" +
+                            "           <img alt=" + ls.document_type + " src=\"static/image/" + ls.document_type + ".png\" height=\"60\" width=\"60\"/>" +
+                            "           <p>" + ls.template_name + "</p>" +
+                            "           <div class=\"caption\" style=\"display: none;\">" +
+                            "               <p></p>" +
+                            "               <p>" +
+                            "                   <a class=\"btn btn-primary\" href=\"javascript:void(0);\" onclick=\"template(" + ls.abk_id + ");\">下载</a>" +
+                            "                   <a data-toggle=\"modal\" href=\"#uploadModal\" class=\" btn btn-primary\"  >更新</a>" +
+                            "               </p>" +
+                            "           </div>" +
+                            "       </div>" +
+                            "   </div>";
+                        if ((i+1)%3==0&&(i<(result.data.length-1))) {
+                            group +=divStart+div+divEnd;
+                            div = "";
+                        }
+                        if(i == result.data.length-1){
+                            group = "<p><em>"+ls.group_name+" 模板</em></em></p>" + group+divStart+div+divEnd;
+                        }
+                    }
                 }
+                $("#template").append(group);
+                $(".thumbnail").hover(function () {
+                    $(this).children("div").css("display", "block");
+                }, function () {
+                    $(this).children("div").css("display", "none");
+                });
             }
-
         }else{
             $('#waring').append("<h5  style='color: red'>"+result.msg+"</h5>");
         }
